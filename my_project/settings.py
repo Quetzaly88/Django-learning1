79a48b7031f9ba35be288323abb9f765b4abbde0
Andrea Nilsson <andreaycoral@icloud.com>
2024-10-14 03:51:24 +0000

    Add views to hello_world/views.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)*!wx(e2zz&u-y+7i7me8k3=$4x@(!df@#n1pc9)$*8l^t8z(x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-quetzaly88-djangolearni-iu37sbtioi4.ws.codeinstitute-ide.net',
 '.herokuapp.com']
